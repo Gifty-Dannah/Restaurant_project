fd28e3145041917b5fc0c22836ea30df19146e12
Gifty-Dannah <ndannahgifty@gmail.com>
2025-09-08 18:24:01 +0000

    fixed my icons

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ app.add_static_files("/assets", "assets")
 
 #  link external icons to the head
 ui.add_head_html('''
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/facebook-nodejs-business-sdk/23.0.1/umd.js"
-                 integrity="sha512-qIG6nSgAim6nCb1j+axhgwt+byAzyIjY5srF0XL2zxTNzNsZETRSgnyaHJwffuOIvltDoNImTrmrrJ82sCLLZg==" 
-                 crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+                s<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
                  ''')
 ui.add_head_html('<link rel="stylesheet" href="/assets/reset.css"/>')
 
--- sections/hero.py
+++ sections/hero.py
@@ -1,17 +1,18 @@
 from nicegui import ui
 
-def render():
-    # big container 
-    with ui.element("div").style("background-image url(/assets/table.jpg").classes("h-screen w-screen flex flex-col bg-cover bg-center justify-center items-center"):
-        # navbar
-        with ui.element("nav").classes("flex flex-row justify-between items-center w-full fixed left-0 top -0 px-20 py-10"):
-            # logo
-            ui.label("LOGO")
 
-            # nav links
+def render():
+   # big container
+    with ui.element("div").style("background-image: url(/assets/table.jpg)").classes("h-screen w-screen flex flex-col bg-cover bg-center justify-center items-center"):
+        # navbar
+        with ui.element("nav").classes("flex flex-row justify-between items-center w-full fixed left-0 top-0 px-20 py-10"):
+            # logo
+            with ui.link(target='/'):
+                ui.html('<i class="fa-solid fa-utensils"></i>').classes('text-white text-4xl')
+            # navlinks  
             with ui.row():
                 navlinks = [
-                {"title": "Home", "path": "/"},
+                {"title": "Home", "path": "/"}, 
                 {"title": "Menu", "path": "/"},
                 {"title": "Gallery", "path": "/"},
                 {"title": "About", "path": "/"},
@@ -19,17 +20,17 @@ def render():
                 {"title": "Contact", "path": "/"},
                 ]
                 for item in navlinks:
-                    ui.link(item["title"], item["path"].classes("no-underline uppercase text-white font-bold"))
+                    ui.link(item["title"], item["path"]).classes("no-underline uppercase text-white font-bold")
 
-            #socials
+            # socials
             with ui.row().classes("text-white"):
-                ui.html('<i class="fa-brands fa-facebook"></i>')
+                ui.html('<i class="fa-brands fa-facebook-f"></i>')
                 ui.html('<i class="fa-brands fa-instagram"></i>')
-                ui.html('<i class="fa-brands fa-threads"></i>')
                 ui.html('<i class="fa-brands fa-x-twitter"></i>')
-
-            # Text 
-            with ui.element("div").classes('text-whote font-bold text-center bg-black/70 f-full w-full flex flex-col items-center jsutify-center'):
-                ui.label("Wilkommen im").classes("text-5xl")
-                ui.html("<h1>Dannah's Restaurant</h1>") 
-                ui.button("Our Menu", color="white-800").classes("")
+        
+        # text
+        with ui.element("div").classes('text-white font-bold text-center bg-black/70 h-full w-full flex flex-col items-center justify-center'): 
+            ui.label("Welcome to").classes("text-5xl")
+            ui.html("<h1>Dannah's Diner</h1>").classes("text-7xl uppercase")
+            ui.button("Our Menu", color="red-800").classes("")
+            
